82699d9adc7d971a2467162a3eda2b67ea715e0b
Nirezi <nirezi@nirezi.jp>
2024-03-15 06:16:52 +0900

    締切日の入力フォーマットを変更

--- extensions/manage_task.py
+++ extensions/manage_task.py
@@ -1,4 +1,5 @@
 import datetime
+import re
 import sqlite3
 
 import discord
@@ -10,13 +11,48 @@ class ManageTask(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @staticmethod
+    async def convert_to_datetime(date: str) -> datetime.datetime:
+        """Convert date string(YYYYMMDD or MMDD or DD) to datetime object"""
+        now = datetime.datetime.now()
+        year = now.year
+        month = now.month
+
+        if len(date) == 8:
+            year = int(date[:4])
+            month = int(date[4:6])
+            day = int(date[6:])
+        elif len(date) == 4:
+            month = int(date[:2])
+            day = int(date[2:])
+        else:  # len(date) == 2
+            day = int(date)
+
+        return datetime.datetime(year, month, day, 9, 00)
+
     @app_commands.command(name="add", description="Add a task")
     @app_commands.guild_only()
     async def add(self, interaction: discord.Interaction, title: str, description: str, deadline: str) -> None:
+        match = re.search(r'\d{2,8}', deadline)
+        if not match:
+            await interaction.response.send_message("締切日の形式が不正です。20241231(2024年12月31日)の形式で入力してください。")
+            return
+
+        try:
+            deadline_datetime = await self.convert_to_datetime(match.group())
+        except ValueError as e:
+            await interaction.response.send_message(str(e))
+            return
+
+        # check deadline is not past
+        if deadline_datetime < datetime.datetime.now():
+            await interaction.response.send_message("締切日が過去の日付です。")
+            return
+
         conn = sqlite3.connect('tasks.db')
         cursor = conn.cursor()
         cursor.execute('INSERT INTO tasks VALUES (?, ?, ?, ?)',
-                       (interaction.id, title, description, deadline))
+                       (interaction.id, title, description, deadline_datetime.timestamp()))
         conn.commit()
         conn.close()
         await interaction.response.send_message("Task added")
@@ -25,10 +61,9 @@ class ManageTask(commands.Cog):
     @app_commands.guild_only()
     async def _list(self, interaction: discord.Interaction) -> None:
         now = datetime.datetime.now()
-        now_unix = now.timestamp()
         conn = sqlite3.connect('tasks.db')
         cursor = conn.cursor()
-        cursor.execute('SELECT * FROM tasks WHERE deadline > ?', (now_unix,))
+        cursor.execute('SELECT * FROM tasks WHERE deadline > ?', (now.timestamp(),))
         tasks = cursor.fetchall()
         conn.close()
         await interaction.response.send_message("\n".join(str(task) for task in tasks)+" tasks")
