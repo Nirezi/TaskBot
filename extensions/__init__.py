6cdd6acb60444f8964ec894ab973ef657a4f8a46
Nirezi <nirezi@nirezi.jp>
2024-03-20 05:33:36 +0900

    cogを読み込ませるように

--- extensions/__init__.py
+++ extensions/__init__.py
@@ -1,6 +1,8 @@
 from discord.ext import commands
 from extensions.manage_task import setup as manage_task_setup
+from extensions.check_task import setup as check_task_setup
 
 
 async def setup(bot: commands.Bot) -> None:
     await manage_task_setup(bot)
+    await check_task_setup(bot)
